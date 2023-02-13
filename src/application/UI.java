075f1a89e45d597dcd9398241dfdfeb2a86c0c79
Gabriel Kruger <krugergabriel27@outlook.com>
2023-02-13 16:24:06 -0300

    Implementing possible moves of King

--- src/application/UI.java
+++ src/application/UI.java
@@ -55,7 +55,7 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 	
-	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) { // Overload
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
